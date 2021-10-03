7152ef9867870a6bcaeba9880b80179700df7b31
Sunwish <770168287@qq.com>
2021-10-03 09:56:30 +0800

    Site updated: 2021-10-03 09:56:22

--- js/src/pvuvfix.js
+++
@@ -1,2 +0,0 @@
-$(".post-meta-divider").html("Just for test");
-$("#busuanzi_container_site_uv").html("Just for fun");
--- pvuvfix.js
+++
@@ -1,3 +0,0 @@
-(function(){
-	    $(".post-meta-divider").html("Just for test");
-	})();<link rel="stylesheet" href="/css/spoiler.css" type="text/css"><script src="/js/spoiler.js" type="text/javascript" async></script>
