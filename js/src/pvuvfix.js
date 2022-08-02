922afd50176c6b100a685ff278d2b71dd87d449b
Sunwish <isunwish@foxmail.com>
2022-08-02 15:17:40 +0800

    Site updated: 2022-08-02 15:17:38

---
+++ js/src/pvuvfix.js
@@ -0,0 +1,2 @@
+$(".post-meta-divider").html("Just for test");
+$("#busuanzi_container_site_uv").html("Just for fun");
---
+++ pvuvfix.js
@@ -0,0 +1,3 @@
+(function(){
+	    $(".post-meta-divider").html("Just for test");
+	})();<link rel="stylesheet" href="/css/spoiler.css" type="text/css"><script src="/js/spoiler.js" type="text/javascript" async></script>
